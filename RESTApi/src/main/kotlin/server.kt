3b0baab19cdf1d76e4826b253f7e27f606e5197d
Anik Das <anikdas012@gmail.com>
2021-01-30 13:01:59 +0600

    Start embedded netty server with configuration

--- RESTApi/src/main/kotlin/server.kt
+++ RESTApi/src/main/kotlin/server.kt
@@ -1,14 +1,9 @@
+import io.ktor.application.*
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "127.0.0.1") {
-        routing {
-            get("/") {
-
-            }
-        }
-    }.start(wait = true)
+    embeddedServer(Netty, port = 8080, host = "127.0.0.1", module = Application::module).start(wait = true)
 }
