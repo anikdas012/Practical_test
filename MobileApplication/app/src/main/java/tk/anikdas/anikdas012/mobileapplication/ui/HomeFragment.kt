49ff8b6df21a827478a78474bdb2bc9c2b757126
Anik Das <anikdas012@gmail.com>
2021-01-30 15:55:03 +0600

    Return home fragment view

--- MobileApplication/app/src/main/java/tk/anikdas/anikdas012/mobileapplication/ui/HomeFragment.kt
+++ MobileApplication/app/src/main/java/tk/anikdas/anikdas012/mobileapplication/ui/HomeFragment.kt
@@ -1,6 +1,11 @@
 package tk.anikdas.anikdas012.mobileapplication.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import tk.anikdas.anikdas012.mobileapplication.databinding.FragmentHomeBinding
 
 /**
  * Created by "Anik Das" on 30-Jan-2021
@@ -8,4 +13,16 @@ import androidx.fragment.app.Fragment
  */
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
+    }
 }
