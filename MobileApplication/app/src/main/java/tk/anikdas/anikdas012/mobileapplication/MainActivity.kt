ac42f3a3da2fc2c1d68cbedf094ce0e5ae5f4176
Anik Das <anikdas012@gmail.com>
2021-01-30 19:09:03 +0600

    Instantiate FragmentComponent

--- MobileApplication/app/src/main/java/tk/anikdas/anikdas012/mobileapplication/MainActivity.kt
+++ MobileApplication/app/src/main/java/tk/anikdas/anikdas012/mobileapplication/MainActivity.kt
@@ -2,10 +2,16 @@ package tk.anikdas.anikdas012.mobileapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import tk.anikdas.anikdas012.mobileapplication.di.FragmentComponent
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var fragmentComponent: FragmentComponent
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fragmentComponent = (application as BaseApplication).appComponent
+            .fragmentComponent().build()
     }
 }
