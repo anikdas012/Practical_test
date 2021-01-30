4cf2a7af029d69d7487ea7e4472181e767065886
Anik Das <anikdas012@gmail.com>
2021-01-30 18:08:23 +0600

    Create new interface for AppComponent

---
+++ MobileApplication/app/src/main/java/tk/anikdas/anikdas012/mobileapplication/di/AppComponent.kt
@@ -0,0 +1,16 @@
+package tk.anikdas.anikdas012.mobileapplication.di
+
+import dagger.Component
+import javax.inject.Singleton
+
+/**
+ * Created by "Anik Das" on 30-Jan-2021
+ * Developer email: "anikdas012@gmail.com"
+ */
+
+@Singleton
+@Component(
+    modules = []
+)
+interface AppComponent {
+}
