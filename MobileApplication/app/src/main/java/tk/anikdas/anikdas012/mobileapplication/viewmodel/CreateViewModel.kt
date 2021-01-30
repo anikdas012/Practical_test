b1d5044ef5c87c080ff90dcb76e133fc4a1af4a4
Anik Das <anikdas012@gmail.com>
2021-01-30 18:56:18 +0600

    Add new class for create viewmodel

---
+++ MobileApplication/app/src/main/java/tk/anikdas/anikdas012/mobileapplication/viewmodel/CreateViewModel.kt
@@ -0,0 +1,14 @@
+package tk.anikdas.anikdas012.mobileapplication.viewmodel
+
+import androidx.lifecycle.ViewModel
+import tk.anikdas.anikdas012.mobileapplication.network.CreateApi
+import javax.inject.Inject
+
+/**
+ * Created by "Anik Das" on 30-Jan-2021
+ * Developer email: "anikdas012@gmail.com"
+ */
+
+class CreateViewModel @Inject constructor(private val createApi: CreateApi): ViewModel() {
+
+}
