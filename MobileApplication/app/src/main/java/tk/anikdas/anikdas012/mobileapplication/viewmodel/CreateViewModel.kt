8f1c1909a8c947e4cd755a2a76d13c9ba1bb2360
Anik Das <anikdas012@gmail.com>
2021-01-30 19:05:22 +0600

    Add method to create user

--- MobileApplication/app/src/main/java/tk/anikdas/anikdas012/mobileapplication/viewmodel/CreateViewModel.kt
+++ MobileApplication/app/src/main/java/tk/anikdas/anikdas012/mobileapplication/viewmodel/CreateViewModel.kt
@@ -1,6 +1,10 @@
 package tk.anikdas.anikdas012.mobileapplication.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.LiveDataReactiveStreams
 import androidx.lifecycle.ViewModel
+import io.reactivex.schedulers.Schedulers
+import tk.anikdas.anikdas012.mobileapplication.models.UserDetails
 import tk.anikdas.anikdas012.mobileapplication.network.CreateApi
 import javax.inject.Inject
 
@@ -11,4 +15,14 @@ import javax.inject.Inject
 
 class CreateViewModel @Inject constructor(private val createApi: CreateApi): ViewModel() {
 
+    fun createUser(user: UserDetails): LiveData<UserDetails> {
+        return LiveDataReactiveStreams.fromPublisher(
+            createApi.createAccount(user)
+                .onErrorReturn {
+                    user.createdAt = -1
+                    return@onErrorReturn user
+                }
+                .subscribeOn(Schedulers.io())
+        )
+    }
 }
