906f9289eb74836af02b810b6241d1a5c8569af7
Anik Das <anikdas012@gmail.com>
2021-01-30 21:23:01 +0600

    Change base url as it is tested through emulator

--- MobileApplication/app/src/main/java/tk/anikdas/anikdas012/mobileapplication/di/AppModule.kt
+++ MobileApplication/app/src/main/java/tk/anikdas/anikdas012/mobileapplication/di/AppModule.kt
@@ -5,7 +5,6 @@ import dagger.Provides
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
-import tk.anikdas.anikdas012.mobileapplication.network.CreateApi
 import javax.inject.Singleton
 
 /**
@@ -23,7 +22,7 @@ object AppModule {
     @Provides
     fun provideRetrofitInstance() : Retrofit {
         return Retrofit.Builder()
-            .baseUrl("http://127.0.0.1:8080/api/")
+            .baseUrl("http://10.0.2.2:8080/api/")
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
