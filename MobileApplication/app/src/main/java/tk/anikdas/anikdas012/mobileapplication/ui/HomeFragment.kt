93dd4a20748a5412da7c9e0064caf2bb82d6ebc6
Anik Das <anikdas012@gmail.com>
2021-01-30 15:56:18 +0600

    Override onViewCreated method

--- MobileApplication/app/src/main/java/tk/anikdas/anikdas012/mobileapplication/ui/HomeFragment.kt
+++ MobileApplication/app/src/main/java/tk/anikdas/anikdas012/mobileapplication/ui/HomeFragment.kt
@@ -25,4 +25,8 @@ class HomeFragment : Fragment() {
         binding = FragmentHomeBinding.inflate(layoutInflater)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
