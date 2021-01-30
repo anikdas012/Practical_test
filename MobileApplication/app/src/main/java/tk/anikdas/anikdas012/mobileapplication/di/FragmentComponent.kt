b491a65f586bcd9a78d4d492b654affe3f726ae1
Anik Das <anikdas012@gmail.com>
2021-01-30 18:41:15 +0600

    Create subcomponent for fragments

---
+++ MobileApplication/app/src/main/java/tk/anikdas/anikdas012/mobileapplication/di/FragmentComponent.kt
@@ -0,0 +1,13 @@
+package tk.anikdas.anikdas012.mobileapplication.di
+
+import dagger.Subcomponent
+
+/**
+ * Created by "Anik Das" on 30-Jan-2021
+ * Developer email: "anikdas012@gmail.com"
+ */
+
+@FragmentScope
+@Subcomponent
+interface FragmentComponent {
+}
