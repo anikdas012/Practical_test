b502a31ccfec98c15a0e5416b06300022ee74089
Anik Das <anikdas012@gmail.com>
2021-01-31 01:29:20 +0600

    Create test class for CreateViewModel

---
+++ MobileApplication/app/src/test/java/tk/anikdas/anikdas012/mobileapplication/viewmodel/CreateViewModelTest.kt
@@ -0,0 +1,27 @@
+package tk.anikdas.anikdas012.mobileapplication.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import io.reactivex.Flowable
+import io.reactivex.Single
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
+import tk.anikdas.anikdas012.mobileapplication.getOrAwaitValueTest
+import tk.anikdas.anikdas012.mobileapplication.models.UserDetails
+import tk.anikdas.anikdas012.mobileapplication.network.CreateApi
+
+/**
+ * Created by "Anik Das" on 31-Jan-2021
+ * Developer email: "anikdas012@gmail.com"
+ */
+
+@RunWith(JUnit4::class)
+class CreateViewModelTest {
+}
