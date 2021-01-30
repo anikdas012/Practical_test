041ec70247dbab6e426268c69f1676c4c4814765
Anik Das <anikdas012@gmail.com>
2021-01-30 16:51:00 +0600

    Add login click listener

--- MobileApplication/app/src/main/java/tk/anikdas/anikdas012/mobileapplication/ui/ForgotPasswordFragment.kt
+++ MobileApplication/app/src/main/java/tk/anikdas/anikdas012/mobileapplication/ui/ForgotPasswordFragment.kt
@@ -1,10 +1,13 @@
 package tk.anikdas.anikdas012.mobileapplication.ui
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import tk.anikdas.anikdas012.mobileapplication.R
 import tk.anikdas.anikdas012.mobileapplication.databinding.FragmentForgotPasswordBinding
 
 /**
@@ -24,5 +27,13 @@ class ForgotPasswordFragment : Fragment() {
         super.onCreateView(inflater, container, savedInstanceState)
         binding = FragmentForgotPasswordBinding.inflate(layoutInflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.loginTextview.setOnClickListener {
+            Navigation.findNavController(this.activity as Activity, R.id.nav_host_fragment)
+                .navigate(R.id.loginFragment)
+        }
     }
 }
