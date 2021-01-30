7c1a6ed7c18c6f3372ade3b4236e459b9c918499
Anik Das <anikdas012@gmail.com>
2021-01-30 18:25:49 +0600

    Create base applictaion class

---
+++ MobileApplication/app/src/main/java/tk/anikdas/anikdas012/mobileapplication/BaseAplication.kt
@@ -0,0 +1,19 @@
+package tk.anikdas.anikdas012.mobileapplication
+
+import android.app.Application
+import tk.anikdas.anikdas012.mobileapplication.di.AppComponent
+import tk.anikdas.anikdas012.mobileapplication.di.DaggerAppComponent
+
+/**
+ * Created by "Anik Das" on 30-Jan-2021
+ * Developer email: "anikdas012@gmail.com"
+ */
+
+class BaseAplication : Application() {
+
+    val appComponent: AppComponent by lazy {
+        DaggerAppComponent.builder()
+            .application(this)
+            .build()
+    }
+}
