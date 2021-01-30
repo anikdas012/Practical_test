778f47ac906dc9d195d3767b56a670d6485a2674
Anik Das <anikdas012@gmail.com>
2021-01-30 14:33:29 +0600

    Initial commit

---
+++ MobileApplication/app/src/main/java/tk/anikdas/anikdas012/mobileapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package tk.anikdas.anikdas012.mobileapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
