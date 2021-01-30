00f526ffd242bcf03c4793bf88cdba07779296c2
Anik Das <anikdas012@gmail.com>
2021-01-30 15:22:56 +0600

    Create new fragment for home screen

---
+++ MobileApplication/app/src/main/java/tk/anikdas/anikdas012/mobileapplication/ui/HomeFragment.kt
@@ -0,0 +1,11 @@
+package tk.anikdas.anikdas012.mobileapplication.ui
+
+import androidx.fragment.app.Fragment
+
+/**
+ * Created by "Anik Das" on 30-Jan-2021
+ * Developer email: "anikdas012@gmail.com"
+ */
+
+class HomeFragment : Fragment() {
+}
