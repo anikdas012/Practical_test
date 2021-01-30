26ce84790fd780e84aaa01b4bed2febe831cc337
Anik Das <anikdas012@gmail.com>
2021-01-31 02:30:25 +0600

    Initialize viewmodel

--- MobileApplication/app/src/test/java/tk/anikdas/anikdas012/mobileapplication/viewmodel/LoginViewModelTest.kt
+++ MobileApplication/app/src/test/java/tk/anikdas/anikdas012/mobileapplication/viewmodel/LoginViewModelTest.kt
@@ -2,9 +2,13 @@ package tk.anikdas.anikdas012.mobileapplication.viewmodel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Rule
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import tk.anikdas.anikdas012.mobileapplication.network.LoginApi
 
 /**
  * Created by "Anik Das" on 31-Jan-2021
@@ -15,4 +19,13 @@ import org.junit.runners.JUnit4
 class LoginViewModelTest {
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
+    @Mock
+    lateinit var loginApi: LoginApi
+    lateinit var viewModel: LoginViewModel
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        viewModel = LoginViewModel(loginApi)
+    }
 }
