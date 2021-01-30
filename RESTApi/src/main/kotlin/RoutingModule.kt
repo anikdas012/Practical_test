242ce0540eab10206d9a2c608eac2183917a1cd4
Anik Das <anikdas012@gmail.com>
2021-01-30 13:08:25 +0600

    Add method to get database connection

--- RESTApi/src/main/kotlin/RoutingModule.kt
+++ RESTApi/src/main/kotlin/RoutingModule.kt
@@ -2,11 +2,17 @@ import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import org.jetbrains.exposed.sql.Database
 
 fun Route.api() {
+    val db: Database = connectDB()
     route("/api") {
         get("/") {
             call.respond(HttpStatusCode.OK, "HTML::index")
         }
     }
+}
+
+fun connectDB(): Database {
+    TODO("Not yet implemented")
 }
