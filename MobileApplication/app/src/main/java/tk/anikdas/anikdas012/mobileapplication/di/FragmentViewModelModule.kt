3deeddf382c1ea48e31976735ba652d5de58ea1b
Anik Das <anikdas012@gmail.com>
2021-01-30 19:33:46 +0600

    Create new module to provide viewmodels

---
+++ MobileApplication/app/src/main/java/tk/anikdas/anikdas012/mobileapplication/di/FragmentViewModelModule.kt
@@ -0,0 +1,12 @@
+package tk.anikdas.anikdas012.mobileapplication.di
+
+import dagger.Module
+
+/**
+ * Created by "Anik Das" on 30-Jan-2021
+ * Developer email: "anikdas012@gmail.com"
+ */
+
+@Module
+class FragmentViewModelModule {
+}
