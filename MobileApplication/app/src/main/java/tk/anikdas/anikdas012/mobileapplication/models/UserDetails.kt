dd8d97b4d92c20b8f7cb7ff8b94413999a2f7c4c
Anik Das <anikdas012@gmail.com>
2021-01-30 18:19:14 +0600

    Create new model class for user details

---
+++ MobileApplication/app/src/main/java/tk/anikdas/anikdas012/mobileapplication/models/UserDetails.kt
@@ -0,0 +1,16 @@
+package tk.anikdas.anikdas012.mobileapplication.models
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Created by "Anik Das" on 30-Jan-2021
+ * Developer email: "anikdas012@gmail.com"
+ */
+
+data class UserDetails(
+    @SerializedName("name") val username: String,
+    @SerializedName("email") val email: String,
+    @SerializedName("password") val password: String,
+    @SerializedName("confirmPassword") val confirmPassword: String,
+    @SerializedName("createdAt") val createdAt: Long?
+)
