725af55d8a50b357b333f65450f8cb29c6a3ed9f
Anik Das <anikdas012@gmail.com>
2021-01-31 02:37:26 +0600

    Create new class for ResetPasswordViewModelTest

---
+++ MobileApplication/app/src/test/java/tk/anikdas/anikdas012/mobileapplication/viewmodel/ResetPasswordViewModelTest.kt
@@ -0,0 +1,15 @@
+package tk.anikdas.anikdas012.mobileapplication.viewmodel
+
+import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+/**
+ * Created by "Anik Das" on 31-Jan-2021
+ * Developer email: "anikdas012@gmail.com"
+ */
+
+@RunWith(JUnit4::class)
+class ResetPasswordViewModelTest {
+    
+}
