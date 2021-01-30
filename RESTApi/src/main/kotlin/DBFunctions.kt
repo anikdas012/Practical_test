7a2172196b09fc4eac36e231745ae342ad2c02df
Anik Das <anikdas012@gmail.com>
2021-01-30 13:17:52 +0600

    Insert user to database

--- RESTApi/src/main/kotlin/DBFunctions.kt
+++ RESTApi/src/main/kotlin/DBFunctions.kt
@@ -1,5 +1,18 @@
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.transactions.transaction
 
 fun addUser(user: User, db: Database): String {
     user.pass = user.pass!!.sha256()
+    var result: String? = null
+    transaction(db) {
+        val sql = "INSERT INTO User (UserName, Email, PasswordHash, CreatedAt) VALUES (\"${user.name}\", \"${user.email}\", \"${user.pass}\", ${user.time});"
+        try {
+            exec(sql)
+            result = "Success"
+        } catch (e: Exception) {
+            result = "Failed"
+        }
+
+    }
+    return result!!
 }
