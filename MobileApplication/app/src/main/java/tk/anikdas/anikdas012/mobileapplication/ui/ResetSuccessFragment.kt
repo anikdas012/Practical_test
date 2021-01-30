0b2e532a37093adf256c1ce87946ebd8448f8f25
Anik Das <anikdas012@gmail.com>
2021-01-30 17:19:24 +0600

    Return view

--- MobileApplication/app/src/main/java/tk/anikdas/anikdas012/mobileapplication/ui/ResetSuccessFragment.kt
+++ MobileApplication/app/src/main/java/tk/anikdas/anikdas012/mobileapplication/ui/ResetSuccessFragment.kt
@@ -1,6 +1,11 @@
 package tk.anikdas.anikdas012.mobileapplication.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import tk.anikdas.anikdas012.mobileapplication.databinding.FragmentResetSuccessBinding
 
 /**
  * Created by "Anik Das" on 30-Jan-2021
@@ -8,4 +13,16 @@ import androidx.fragment.app.Fragment
  */
 
 class ResetSuccessFragment : Fragment() {
+
+    private lateinit var binding: FragmentResetSuccessBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentResetSuccessBinding.inflate(layoutInflater)
+        return binding.root
+    }
 }
