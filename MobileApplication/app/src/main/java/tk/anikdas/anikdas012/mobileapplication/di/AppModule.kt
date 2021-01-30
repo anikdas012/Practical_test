6ca097e595426af6eb1b1808b31bffe19ed0f739
Anik Das <anikdas012@gmail.com>
2021-01-30 18:11:42 +0600

    Add method to provide retrofit instance

--- MobileApplication/app/src/main/java/tk/anikdas/anikdas012/mobileapplication/di/AppModule.kt
+++ MobileApplication/app/src/main/java/tk/anikdas/anikdas012/mobileapplication/di/AppModule.kt
@@ -1,6 +1,11 @@
 package tk.anikdas.anikdas012.mobileapplication.di
 
 import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 /**
  * Created by "Anik Das" on 30-Jan-2021
@@ -9,4 +14,15 @@ import dagger.Module
 
 @Module
 object AppModule {
+
+    @Singleton
+    @JvmStatic
+    @Provides
+    fun provideRetrofitInstance() : Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("127.0.0.1:8080/api")
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 }
