5d85fcd3fe7b86bbb131184aa0c801e71f75c8db
Anik Das <anikdas012@gmail.com>
2021-01-30 16:34:31 +0600

    Return view

--- MobileApplication/app/src/main/java/tk/anikdas/anikdas012/mobileapplication/ui/LogInFragment.kt
+++ MobileApplication/app/src/main/java/tk/anikdas/anikdas012/mobileapplication/ui/LogInFragment.kt
@@ -1,6 +1,11 @@
 package tk.anikdas.anikdas012.mobileapplication.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import tk.anikdas.anikdas012.mobileapplication.databinding.FragmentLoginBinding
 
 /**
  * Created by "Anik Das" on 30-Jan-2021
@@ -8,4 +13,16 @@ import androidx.fragment.app.Fragment
  */
 
 class LogInFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentLoginBinding.inflate(layoutInflater)
+        return binding.root
+    }
 }
