0366c7702398d2e6165b3c580b2e87fd65305c83
Anik Das <anikdas012@gmail.com>
2021-01-30 13:13:41 +0600

    Add route for creating user

--- RESTApi/src/main/kotlin/RoutingModule.kt
+++ RESTApi/src/main/kotlin/RoutingModule.kt
@@ -1,5 +1,6 @@
 import io.ktor.application.*
 import io.ktor.http.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import org.jetbrains.exposed.sql.Database
@@ -9,8 +10,11 @@ import java.sql.Connection
 fun Route.api() {
     val db: Database = connectDB()
     route("/api") {
-        get("/") {
-            call.respond(HttpStatusCode.OK, "HTML::index")
+        post ("/user") {
+            var user = call.receive<User>()
+            user.time = System.currentTimeMillis()
+            val result = addUser(user, db)
+            call.respond(HttpStatusCode.OK, result)
         }
     }
 }
