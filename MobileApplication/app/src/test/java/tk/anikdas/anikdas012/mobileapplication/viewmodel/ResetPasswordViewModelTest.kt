30fd60f44341b4eb13af48eaa20a449ec433f12a
Anik Das <anikdas012@gmail.com>
2021-01-31 02:39:06 +0600

    Initialize viewmodel

--- MobileApplication/app/src/test/java/tk/anikdas/anikdas012/mobileapplication/viewmodel/ResetPasswordViewModelTest.kt
+++ MobileApplication/app/src/test/java/tk/anikdas/anikdas012/mobileapplication/viewmodel/ResetPasswordViewModelTest.kt
@@ -2,9 +2,13 @@ package tk.anikdas.anikdas012.mobileapplication.viewmodel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Rule
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import tk.anikdas.anikdas012.mobileapplication.network.ResetPasswordApi
 
 /**
  * Created by "Anik Das" on 31-Jan-2021
@@ -15,4 +19,13 @@ import org.junit.runners.JUnit4
 class ResetPasswordViewModelTest {
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
+    @Mock
+    lateinit var resetPasswordApi: ResetPasswordApi
+    lateinit var viewModel: ResetPasswordViewModel
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        viewModel = ResetPasswordViewModel(resetPasswordApi)
+    }
 }
